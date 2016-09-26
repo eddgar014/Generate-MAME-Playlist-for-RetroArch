ec53a22f58ff8b903720f399caad14cb2c811bc4
Mike <legacy5k@gmail.com>
2016-09-26 19:46:29 -0400

    Add files via upload

---
+++ Generate MAME Playlist for RetroArch.ps1
@@ -0,0 +1,87 @@
+
+#
+#              Script: Generate MAME Playlist for RetroArch
+#              Author: singularity098
+#                Date: 2016-09-26
+#            Revision: v1.0
+#   Last Revised Date: 2016-09-26
+#
+
+# Supply info as appropriate to your collection
+$mameRomExtension = '.zip'
+$mameRomDir = 'Z:\ROMs\MAME ROMs [TorrentZipped-Split]\'
+$databaseInputFile = '.\MAME 177 Full.dat'
+$playlistOutputFile = '.\MAME.lpl'
+
+# Some optional criteria which will results in more or less ROMs being added to the playlist
+$minYear = "1988"
+$maxYear = "1998"
+$checkIfRomsExist = $true
+$excludeClones = $true
+
+if (!(test-path -literalpath $databaseInputFile)) {
+	write-host ""
+	write-host " -=ERROR=-  Terminating script because unable to locate input file: `"$databaseInputFile`""
+	write-host ""
+	sleep 1
+	exit
+}
+
+write-host ""
+write-host "Loading XML data from file: `"$databaseInputFile`""
+write-host ""
+$mameDat = [xml] (get-content $databaseInputFile)
+
+write-host "Processing ROMs..."
+write-host ""
+
+$x = 0
+$fullPlaylist = $null
+
+$mameDat.datafile.machine | foreach {
+	
+	$processThisGame = $true
+	$fullRomPath = $mameRomDir + $_.Name + $mameRomExtension
+	
+	if (($_.cloneof) -and $excludeClones)            {$processThisGame = $false}
+	if (!($_.driver.status -eq "Good"))              {$processThisGame = $false}
+	if ($_.description -like "*Player's Edge Plus*") {$processThisGame = $false}
+	if ($_.year -lt $minYear)                        {$processThisGame = $false}
+	if ($_.year -gt $maxYear)                        {$processThisGame = $false}
+	
+	if ($checkIfRomsExist -and $processThisGame) {
+		if (!(test-path -literalpath $fullRomPath)) {
+			$processThisGame = $false
+		}
+	}
+	
+	if ($processThisGame) {
+	
+		$x = $x + 1
+		write-host $x.ToString("0000") `| $_.Name.PadLeft(8) `| $_.Description
+		
+		$fullPlaylist = $fullPlaylist + $fullRomPath + "`n"
+		$fullPlaylist = $fullPlaylist + $_.Description + "`n"
+		$fullPlaylist = $fullPlaylist + "DETECT" + "`n"
+		$fullPlaylist = $fullPlaylist + "DETECT" + "`n"
+		$fullPlaylist = $fullPlaylist + $x.ToString("00000000") + "`|crc`n"
+		$fullPlaylist = $fullPlaylist + "MAME.lpl" + "`n"
+		
+	}
+}
+
+if ($x -eq 0) {
+	write-host " -=ERROR=-  No ROMs found"
+	write-host ""
+	sleep 1
+	exit
+}
+
+write-host ""
+write-host "Writing data to playlist file: `"$playlistOutputFile`""
+write-host ""
+
+$fullPlaylist | out-file $playlistOutputFile -encoding utf8
+
+write-host "Finished."
+write-host ""
